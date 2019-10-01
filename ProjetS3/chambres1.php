c42e5a09eabf3b4ee32dc065533a1dd31674f830
Clément ROUY <49551709+rouyc@users.noreply.github.com>
2019-10-01 10:16:26 +0200

    Add files via upload

---
+++ ProjetS3/chambres1.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Location chambre d'hôtes</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="./styles.css">
+  </head>
+
+  <body>
+    <div class="container" id="wrapper">
+      
+      <!-- Menu -->
+      <div class="row">
+        <div class="col s12">
+          <div class="card">
+            <div class="card-content">
+                <div> <a class="menu" href="./index.html"> Accueil </a> </div>
+                <div> <a class="menu" href="./formulaireChambre.html"> Admin </a> </div>
+                <div> <a class="menu" href="./lireChambre.php"> Chambre description test </a> </div>
+                <div> <a class="menu" href="./chambres1.php"> Chambre </a> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Caroussel -->
+      <div class="row">
+        <div class="col s12">
+          <div class="card">
+            <div class="card-content ">
+              <div class="center">
+                <img src="images/1.jpg" alt="n1">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Chambres d'hôtes -->
+      <div class="row">
+        <h2> Nos chambres d'hôtes </h2>
+        <div class="col s12">
+          <div class="card">
+            <div class="card-content">
+              <?php
+                require_once "Model.php";
+                require_once "Chambre.php";
+                
+                $desc = Chambre::getDescription();
+
+                foreach  ($desc as $obj) {
+                    echo $obj->afficher();
+                }
+              ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+
+  <footer>
+  </footer>
+</html>
